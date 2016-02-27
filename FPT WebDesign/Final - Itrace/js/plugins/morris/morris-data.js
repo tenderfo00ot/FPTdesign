1944d373e993f9af442b6cd6de13bcc928733ad0
Evan Lopez <tenderfo00ot@gmail.com>
2016-02-28 04:00:55 +0800

    Trace-vote UI (phase 2)

--- FPT WebDesign/Final - Itrace/js/plugins/morris/morris-data.js
+++ FPT WebDesign/Final - Itrace/js/plugins/morris/morris-data.js
@@ -1,8 +1,22 @@
-Morris.Donut({
-  element: 'donut-example',
+Morris.Bar({
+  element: 'bar-example',
   data: [
-    {label: "Voted", value: 80},
-    {label: "Not Voted", value: 20}]
+    { y: '2006', a: 100, b: 90 },
+    { y: '2007', a: 75,  b: 65 },
+    { y: '2008', a: 50,  b: 40 },
+    { y: '2009', a: 75,  b: 65 },
+    { y: '2010', a: 50,  b: 40 },
+    { y: '2011', a: 75,  b: 65 },
+    { y: '2012', a: 100, b: 90 }
+
+  ],
+  xkey: 'y',
+  ykeys: ['a', 'b'],
+  labels: ['Series A', 'Series B'],
+  resize: true
+}).on('click',function(i,row){
+  console.log(row)
+  console.log(i,row)
 });
 
 Morris.Donut({
